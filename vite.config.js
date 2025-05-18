8eae3d96a8e659f10839902b68c81f1fc80e65b0
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2025-05-18 13:26:41 -0400

    Revert "Fix build"
    This reverts commit d93c801493176960d9a050d8b6ad9b41a725ee85.

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,15 @@ import fs from "fs";
 import path from "path";
 
 export default defineConfig(({ mode }) => {
-  // Pick up VITE_BASE_URL (set in GitHub Actions) or default to root
-  const base = process.env.VITE_BASE_URL || "/";
-  const localHttpsConfig =
-    mode == "development"
-      ? {
-          server: {
-            https: {
-              key: fs.readFileSync(path.resolve(__dirname, "key.pem")),
-              cert: fs.readFileSync(path.resolve(__dirname, "cert.pem")),
-            },
-            host: true, // allows LAN access
+  return mode == "development"
+    ? {
+        server: {
+          https: {
+            key: fs.readFileSync(path.resolve(__dirname, "key.pem")),
+            cert: fs.readFileSync(path.resolve(__dirname, "cert.pem")),
           },
-        }
-      : {};
-  return {
-    base,
-    ...localHttpsConfig,
-  };
+          host: true, // allows LAN access
+        },
+      }
+    : {};
 });
