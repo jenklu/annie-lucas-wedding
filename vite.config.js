d93c801493176960d9a050d8b6ad9b41a725ee85
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2025-05-18 13:19:37 -0400

    Fix build

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,22 @@ import fs from "fs";
 import path from "path";
 
 export default defineConfig(({ mode }) => {
-  return mode == "development"
-    ? {
-        server: {
-          https: {
-            key: fs.readFileSync(path.resolve(__dirname, "key.pem")),
-            cert: fs.readFileSync(path.resolve(__dirname, "cert.pem")),
+  // Pick up VITE_BASE_URL (set in GitHub Actions) or default to root
+  const base = process.env.VITE_BASE_URL || "/";
+  const localHttpsConfig =
+    mode == "development"
+      ? {
+          server: {
+            https: {
+              key: fs.readFileSync(path.resolve(__dirname, "key.pem")),
+              cert: fs.readFileSync(path.resolve(__dirname, "cert.pem")),
+            },
+            host: true, // allows LAN access
           },
-          host: true, // allows LAN access
-        },
-      }
-    : {};
+        }
+      : {};
+  return {
+    base,
+    ...localHttpsConfig,
+  };
 });
