ac45187232bf8a4c5b47f0ec368d2100077b9a17
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2025-05-18 12:42:19 -0400

    Don't use self-signed certs for prod

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from "vite";
 import fs from "fs";
 import path from "path";
 
-export default defineConfig({
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "key.pem")),
-      cert: fs.readFileSync(path.resolve(__dirname, "cert.pem")),
-    },
-    host: true, // allows LAN access
-  },
+export default defineConfig(({ mode }) => {
+  return mode == "development"
+    ? {
+        server: {
+          https: {
+            key: fs.readFileSync(path.resolve(__dirname, "key.pem")),
+            cert: fs.readFileSync(path.resolve(__dirname, "cert.pem")),
+          },
+          host: true, // allows LAN access
+        },
+      }
+    : {};
 });
