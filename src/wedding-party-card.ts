a8378ca1d48b661a886f694eb4212e75c973cd93
Lucas Jenkins <lucas.jenkins.232@gmail.com>
2025-07-15 02:59:14 -0400

    Remove unused

--- src/wedding-party-card.ts
+++ src/wedding-party-card.ts
@@ -1,4 +1,4 @@
-import { LitElement, html, css, type TemplateResult } from 'lit';
+import { LitElement, html, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { paragraphStyle } from './styles';
 
